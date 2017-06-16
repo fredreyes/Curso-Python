dd14cfc7e1c76807d2931fed6deb40a73a8968bc
of-curso-python <of.curso.python@gmail.com>
2017-06-16 09:03:03 -0600

    I-1

--- TAREA.py
+++ TAREA.py
@@ -1,23 +1,11 @@
- #TAREA
- #cREAR UNA APP QUE RECIBA EL NOMBRE DE USUARIO Y SU EDAD E 
- #IMPRIMIR EL AO EN QUE ESE USUARIO VA A CUMPLIR 100 A'
-
-#Saludo = 'HOLA, Ingrese su Nombre'
-#Sedad = "Ahora Ingresa tu edad"
-#Sanio = "Tendras 100 anios en el "
-#anio = 2017
-
-nombre = input("INgrese NOmbre :")
-edad = int(input("ingrese edad :"))
-anio = int(input("INgrese anio actual :"))
+# Tarea:
+# Crear un app que reciba el nombre de usuario y edad e
+# imprimir el año en que ese usuario cumplira 100 años
 
 
 if __name__=='__main__':
-#	print(Saludo)
-#	nombre = input()
-#	print("Mucho Gusto: " , nombre)
-#	print(Sedad)
-#	edad = int(input())
-#	print(Sanio, (edad-100)+ anio)
-	print("Cumpliras 100 anios en : " , (100- edad)+anio )
+    nombre = input("INgrese NOmbre :")
+    edad = int(input("ingrese edad :"))
+    anio = int(input("INgrese anio actual :"))
+    print("Cumpliras 100 anios en : " , (100- edad)+anio )
 
