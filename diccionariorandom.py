b45a8de7e2cb25d9d6ba1f65ddab4bcd6d0889cd
Udem <udem@fedora.mshome.net>
2017-06-26 17:39:40 -0600

    Diccionario Bueno aleatorios

--- diccionariorandom.py
+++ diccionariorandom.py
@@ -40,19 +40,49 @@ def imprimirestudiante():
 
 
 	for llave, valor in dic.iteritems():
-		#print 'el estudiente %s de la edad de %s, es de la ciudad de %s y cursa el anio %s en la universidad' % (llave,valor) 
-		print 'El alumno {alumno} es de la edad {edad} '.format(alumno=llave , edad=valor['edad'])
+            #print 'el estudiente %s de la edad de %s, es de la ciudad de %s y cursa el anio %s en la universidad' % (llave,valor)
+            print 'El alumno {alumno} es de la edad de {edad} ciudad de {cuidad} y cursa el anio{anio}'.format(alumno=llave , edad=valor['edad'], cuidad=valor['cuidad'], anio=valor['anio'])
+def estudianteManagua():
+    dic = generar_diccionario_estudiantes()
 
+    for llave, valor in dic.iteritems():
+        if 'Managua' in valor['cuidad']:
+            print 'EL Alumno {alumno} de la edad de {edad}, es de la ciudad de {cuidad} y cursa el anio {anio} en la Universidad'.format(alumno=llave, edad=valor['edad'], cuidad=valor['cuidad'], anio=valor['anio'])
 
-if __name__ == '__main__':   	
-	imprimirestudiante()
+def  estudiantesMasayaprimeranio():
+    dic = generar_diccionario_estudiantes()
+    x = 1
+    for llave, valor in dic.iteritems():
+        if 'Masaya' == valor['cuidad'] and x == valor['anio']:
+            print 'El alumno {estudiante} de la edad de {ed}, es de la ciudad de {ciudad} y cursa el anio {anio} en la universidad'.format(estudiante=llave, ed=valor['edad'], ciudad=valor['cuidad'], anio=valor['anio'])
+
+def estudiantemenode21():
+    dic = generar_diccionario_estudiantes()
+    
+    for llave, valor in dic.iteritems():
+        if valor['edad'] < 21:
+            print '***EL alumno {estudiante} de la edad de {ed}, es de la ciudad de {ciudad} y cursa el anio {anio} en la Universidad'.format(
+                estudiante=llave,
+                ed=valor['edad'],
+                ciudad=valor['cuidad'],
+                anio=valor['anio'] )
+
+if __name__ == '__main__':
+    print 'TODOS'
+    imprimirestudiante()
+    print 'SOLO MANAGUA'
+    estudianteManagua()
+    print 'SOLO MASAYA DE PRIMER ANIO'
+    estudiantesMasayaprimeranio()
+    print 'SOLO MENORES DE 21 ANIOS'
+    estudiantemenode21()
 
 # - Crear un script que genere el diccionario de estudiantes
 #   usando "generar_diccionario_estudiantes"
 
 # - Imprimir todos las llaves del diccionario generado de la forma:
 #   "El <nombre_estudiante> de <edad>, es de la ciudad de <ciudad> y
-#   cursa <anio> en la universidad"
+#   cursa <anio> en la universidad
 
 # - Del diccionario de estudiantes creado imprimir los estudiantes
 #   de la ciudad de 'Managua' de la forma:
