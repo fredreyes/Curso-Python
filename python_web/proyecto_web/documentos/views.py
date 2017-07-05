e44a52214394b52e90f6b132eb75ef30fd29fdcb
Udem <udem@fedora.mshome.net>
2017-07-05 17:51:54 -0600

    Documentos con vista comleta

--- python_web/proyecto_web/documentos/views.py
+++ python_web/proyecto_web/documentos/views.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.generic import View
+
 from django.shortcuts import render
+from documentos.models import Documento
 
 # Create your views here.
+class Documentos(View):
+	def get(self, request, *arg, **kwargs):
+
+		docs = Documento.objects.all()
+		context ={
+		    'docs': docs,
+		    'encabezado': 'Mis documentos'
+		}
+		return render(
+			request,
+			'documento.html',
+			context
+		)
+		
