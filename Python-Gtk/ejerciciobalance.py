2e5203e32a67d0afd3fc8f16405304008868eb02
Udem <udem@fedora.mshome.net>
2017-07-17 17:51:34 -0600

    Python Gtk Avanzado

--- Python-Gtk/ejerciciobalance.py
+++ Python-Gtk/ejerciciobalance.py
@@ -17,7 +17,9 @@ class VentanaBalance(Gtk.Window):
 		self.agregar_boton_activo()
 		self.agregar_lista_activos()
 		self.agregar_label_Pasivo()
-
+		self.agregar_entradas_pasivos()
+		self.agregar_boton_pasivo()
+		self.agregar_lista_Pasivo()
 
 
 	def agregar_contenedor_activo(self):
@@ -34,7 +36,7 @@ class VentanaBalance(Gtk.Window):
 		self.entradaA = Gtk.Entry()
 		self.montoA = Gtk.Entry()
 		self.contenedor.attach(self.entradaA, 0,1,2,1)
-		self.contenedor.attach(self.montoA, 3,1,1,1)
+		self.contenedor.attach_next_to(self.montoA, self.entradaA, Gtk.PositionType.RIGHT, 1,1)
 
 	def agregar_boton_activo(self):
 		self.boton_activo = Gtk.Button('Agregar Activo')
@@ -46,10 +48,16 @@ class VentanaBalance(Gtk.Window):
 		self.listaA = Gtk.TreeView(self.modeloA)
 
 		descripcionA = Gtk.CellRendererText()
+		descripcionA.set_property('editable', True)
+		descripcionA.connect('edited', self.texto_editado_activo)
 		columna_descripcionA = Gtk.TreeViewColumn('DESCRIPCION', descripcionA, text=0)
-
+		
 		montoA = Gtk.CellRendererText()
+		montoA.set_property('editable', True)
+		montoA.connect('edited', self.monto_editado_activo)
+
 		columna_montoA = Gtk.TreeViewColumn('MONTO ACTIVO', montoA, text=1)
+
 
 		self.listaA.append_column(columna_descripcionA)
 		self.listaA.append_column(columna_montoA)
@@ -61,6 +69,15 @@ class VentanaBalance(Gtk.Window):
 			1,
 			1
 			)
+	#editar datos del modelo
+
+	def texto_editado_activo(self, widget, path, text):
+		self.modeloA[path][0] = text
+
+	def monto_editado_activo(self, widget, path, text):
+		self.modeloA[path][1]= float(text)
+		
+		
 	def agregar_fila_activo(self, btn):
 		texto = self.entradaA.get_text()
 		tmontoA = self.montoA.get_text()
@@ -68,17 +85,64 @@ class VentanaBalance(Gtk.Window):
 		if len(lista) >0:
 			self.modeloA.append([texto,float(tmontoA)])
 
-
-
-			# pasivos
+			# ***********************************pasivos ****************************************************
 
 
 	def agregar_label_Pasivo(self):
 		self.lpasivo = Gtk.Label('Pasivos')
 		self.contenedor.attach_next_to(self.lpasivo, self.listaA, Gtk.PositionType.BOTTOM,1,2)
-		
+	
+	def agregar_entradas_pasivos(self):
+		self.entradaP = Gtk.Entry()
+		self.montoP = Gtk.Entry()
+		self.contenedor.attach(self.entradaP, 0,6,2,1)
+		self.contenedor.attach_next_to(self.montoP, self.entradaP, Gtk.PositionType.RIGHT, 1,1)
+
+	def agregar_boton_pasivo(self):
+		self.botonP = Gtk.Button('AGREGAR PASIVOS')
+		self.contenedor.attach(self.botonP, 1,7,1,1)
+		self.botonP.connect('clicked', self.agregar_fila_pasivo)
+
+	def agregar_lista_Pasivo(self):
+		self.modeloP = Gtk.ListStore(str, float)
+		self.listaP = Gtk.TreeView(self.modeloP)
+
+		descripcionP = Gtk.CellRendererText()
+		descripcionP.set_property('editable', True)
+		descripcionP.connect('edited', self.texto_editado_pasivo)
+
+		columna_descripcionP = Gtk.TreeViewColumn('DESCRIPCION', descripcionP, text=0)
+
+		montoPasivo = Gtk.CellRendererText()
+		montoPasivo.set_property('editable', True)
+		montoPasivo.connect('edited', self.monto_editado_pasivo)
+		columna_montoP = Gtk.TreeViewColumn('MONTO PASIVO', montoPasivo, text = 1)
+
+		self.listaP.append_column(columna_descripcionP)
+		self.listaP.append_column(columna_montoP)
+
+		self.contenedor.attach_next_to(
+			self.listaP,
+			self.botonP,
+			Gtk.PositionType.BOTTOM,
+			1,1)
+
+	def texto_editado_pasivo(self, widget, path, text):
+		self.modeloP[path][0] = text
+
+	def monto_editado_pasivo(self, widget, path, text):
+		self.modeloP[path][1]= float(text)
 
 
+
+
+
+	def agregar_fila_pasivo(self, btn):
+		textpP = self.entradaP.get_text()
+		lmontoP = self.montoP.get_text()
+		listaPA = self.entradaP.get_text()
+		if  len(listaPA) >0:
+			self.modeloP.append([textpP, float(lmontoP)])
 
 
 if __name__ == '__main__':
